6bbdc29bed207584d564f471df6fffbe9d6d64cd
alabador <austinlabador@gmail.com>
2022-06-20 20:37:57 -0700

    Build out player object

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const board = document.querySelector('.gameboard');
 
 const gameboard = (() => {
     const _gameboardArray = [
@@ -7,22 +8,42 @@ const gameboard = (() => {
     ];
 
     function renderGameboard() {
-        const board = document.querySelector('.gameboard');
-        
+        // const board = document.querySelector('.gameboard');
         for (let i=0; i<_gameboardArray.length; i++){
             let currentMarker = _gameboardArray[i];
             let newSquare = document.createElement('div');
             newSquare.classList.add('square');
             newSquare.textContent = currentMarker;
             board.appendChild(newSquare);
-
         }
-    }
+    };
 
-    return {renderGameboard};
+    function updateBoard() {
+
+    };
+
+    return {renderGameboard, updateBoard};
 })();
 
-const player = (name, marker) => {
+const currentPlayer = ((player) => {
 
-    return {name, marker};
+})();
+
+const Player = (name, marker) => {
+    const getName = () => name;
+    const getMarker = () => marker;
+    
+    function allowClick() {
+        board.addEventListener('click', (e) => {
+            console.log(e.target);
+        });
+    };
+
+    return {getName, getMarker, allowClick};
 }
+
+const player1 = Player('player1', 'X');
+// const player2 = Player('player2', 'O');
+
+//Event listeners
+window.onload = gameboard.renderGameboard();
